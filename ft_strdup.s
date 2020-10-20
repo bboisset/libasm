2200731b7ba53f3276b74de0625ae191e90ea3b9
bboisset <49042653+bboisset@users.noreply.github.com>
2020-10-20 11:28:04 +0200

    Update ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -5,8 +5,14 @@
 section .text
 	global ft_strdup ; char *strdup(const char *s)
 	ft_strdup:
-		mov rx, rdi ; save string in other register
+		mov rdx, rdi ; save string in other register
 		call ft_strlen
 		inc rax ; add one for '\0' char
 		call malloc ; rax is the argument
+		jz .ret ; if malloc fail return
+		mov rdi, rax ;
+		mov rsi, rdx
+		call ft_strcpy
 		
+	.ret:
+		ret
