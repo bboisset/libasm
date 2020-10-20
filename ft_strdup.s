29cc0fd89383705def2abf3b9dbc4e699973dee8
bboisset <49042653+bboisset@users.noreply.github.com>
2020-10-20 11:14:31 +0200

    Update ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,7 +1,12 @@
 	extern malloc
 	extern ft_strlen
+	extern ft_strcpy
 
 section .text
 	global ft_strdup ; char *strdup(const char *s)
 	ft_strdup:
+		mov rx, rdi ; save string in other register
+		call ft_strlen
+		inc rax ; add one for '\0' char
+		call malloc ; rax is the argument
 		
