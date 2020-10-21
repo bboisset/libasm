35573865e8d59d05e5617e6ff481e67f57bfde65
BoissetMCP <boisset14000@gmail.com>
2020-10-21 17:42:57 +0200

    maj main

--- libasm.h
+++ libasm.h
@@ -6,6 +6,6 @@
 
 
 size_t	ft_strlen(char *str);
-int		ft_strcpy(char *dst, const char *src);
+char 	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
