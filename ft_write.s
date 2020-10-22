d5cd02fadc1f2808d80fb634f5894bd511e3e8d1
BoissetMCP <boisset14000@gmail.com>
2020-10-22 16:31:15 +0200

    fix write length return

--- ft_write.s
+++ ft_write.s
@@ -1,9 +1,11 @@
 section	.text
 	global	ft_write ; ssize_t 	write(int fd, cont void *buf, size_t count)
 	ft_write:
-		mov rax, 1
+		mov rcx, rdx	; save count value in other register
+		mov rax, 1		; 1 is the value of syscall write
 		syscall
-		jc exit_error
+		jc exit_error	; on syscall error jump to exit_error
+		mov rax, rdx	; return save count in rax for function return
 		ret
 
 exit_error:
