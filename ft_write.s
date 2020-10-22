f2c455941e039efd5cd6df9423400c1ed14c0422
BoissetMCP <boisset14000@gmail.com>
2020-10-22 19:45:33 +0200

    try to fix write

--- ft_write.s
+++ ft_write.s
@@ -2,11 +2,11 @@ section	.text
 	global	ft_write ; ssize_t 	write(int fd, cont void *buf, size_t count)
 
 ft_write:
-		mov rcx, rdx	; save count value in other register
+		mov r8, rdx	; save count value in other register
 		mov rax, 1		; 1 is the value of syscall write
 		syscall
 		jc exit_error	; if syscall return value less than 0
-		mov rax, rcx	; return save count in rax for function return
+		mov rax, r8	; return save count in rax for function return
 		ret
 
 exit_error:
