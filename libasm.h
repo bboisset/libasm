e3882c1d8b5fab610e3cbddc96883df666e0c7dd
BoissetMCP <boisset14000@gmail.com>
2020-10-22 16:16:14 +0200

    fix .h

--- libasm.h
+++ libasm.h
@@ -1,9 +1,11 @@
+#ifndef LIBASM_H
+# define LIBASM_H
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
 
 size_t	ft_strlen(char *str);
 char 	*ft_strcpy(char *dst, const char *src);
@@ -11,3 +13,5 @@ int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
 ssize_t ft_read(int fd, const void *buf, size_t count);
 char    *ft_strdup(const char *s);
+
+#endif
